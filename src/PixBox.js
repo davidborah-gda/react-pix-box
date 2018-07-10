3e5aac2c69131d044b8bca3a0a5c17bb2678fb54
David Borah <dborah87@gmail.com>
2018-07-09 20:44:52 -0400

    Finished the project and adjusted the CSS grid to make the second column row 2 span the entire row.

--- src/PixBox.js
+++ src/PixBox.js
@@ -4,69 +4,30 @@ import './PixBox.css';
 
 
 class PixBox extends Component {
+
   constructor(props) {
     super(props);
     this.state = {
-      color: "white",
-      first: "#FF0000",
-      second: "#0000FF"
+      enlargeSrc:"",
     };
   }
-
-  First() {
-    this.setState({
-      color: this.state.first
-    });
-  }
-  Second() {
-    this.setState({
-      color: this.state.second
-    });
-  }
-
-  changeColor(evt) {
-
-    this.setState({
-      color: "red"
-    });
-  }
-
-  changeFirstColor(evt){
-    this.setState({
-     first: evt.target.value
-    })
-  }
-
-  changeSecondColor(evt){
-    this.setState({
-      second: evt.target.value
-    })
-  }
   
+    selectIMG(e) {
+      this.setState({
+        enlargeSrc: e.target.src
+      })
+    }
+
 
   render() {
-    const styles = { backgroundColor: this.state.color };
+
     return (
-      <div className="PixBox-container">
-        {this.props.title ? <h1 className="PixBox-title">{this.props.title}</h1> : null}
-        <h1 className="box" style={styles}></h1>
-        <section>
-          <button onClick={this.First.bind(this)} className="PixBox-button">First</button>
-          <button onClick={this.Second.bind(this)} className="PixBox-button">Second</button>
-        </section>
-        <section><h3>Choose Your First Color</h3>
-          <input
-            placeholder="HEX Color Code"
-            onChange={this.changeFirstColor.bind(this)}
-            className="PixBox-input"
-            value= {this.state.first} />
-          <h3>Choose Your Second Color</h3>
-          <input
-            placeholder="HEX Color Code"
-            onChange={this.changeSecondColor.bind(this)}
-            className="PixBox-input"
-            value= {this.state.second} />
-        </section>
+      
+      <div className="grid-container">
+                <div className="selection"><img onClick={this.selectIMG.bind(this)} src="http://celebwallpapers.net/wp-content/uploads/2018/02/free-christmas-birds-wallpaper-best-of-cardinal-bird-wallpaper-of-free-christmas-birds-wallpaper-350x150.jpg" /></div>
+                <div className="selection"><img onClick={this.selectIMG.bind(this)} src="http://celebwallpapers.net/wp-content/uploads/2018/02/free-christmas-birds-wallpaper-best-of-cute-bird-wallpaper-birds-photo-animal-of-free-christmas-birds-wallpaper-350x150.jpg" /></div>
+                <div className="selection"><img onClick={this.selectIMG.bind(this)} src="http://celebwallpapers.net/wp-content/uploads/2018/02/free-christmas-birds-wallpaper-free-christmas-bird-wallpaper-christmas-bird-modern-hq-of-free-christmas-birds-wallpaper-350x150.jpg" /></div>
+                <div className="output"><img className="destination" src={this.state.enlargeSrc} /></div>
       </div>
     )
   }
